60ceb1c6ee1c0aa29064c858305dfda075d8fa3d
ayat29 <37980700+ayat29@users.noreply.github.com>
2023-09-29 12:20:17 +0600

    Update examine.py

--- examine.py
+++ examine.py
@@ -1,10 +1,16 @@
 import numpy as np
 import torch
-from geospatial_fm import TemporalViTEncoder
+from geospatial_fm import TemporalViTEncoder, ConvTransformerTokensToEmbeddingNeck
 
 pretrained_path = "/content/drive/MyDrive/Prithvi_weights/Prithvi_100M.pt"
 model = TemporalViTEncoder(pretrained = pretrained_path, embed_dim = 768)
+nodel = ConvTransformerTokensToEmbeddingNeck(embed_dim = 768, output_embed_dim = 768)
+
 example_input_1 = torch.tensor(np.random.randn(1, 3, 1, 224, 224)).float()
 example_input_2 = torch.tensor(np.random.randn(1, 3, 1, 224, 224)).float()
 
-print(model(example_input_1, example_input_2))
+out = model(example_input_1, example_input_2)
+temp = out
+out = torch.cat((out[0][0], out[1][0])).reshape(1, 1, -1, 768)
+print(nodel(temp[0].reshape(1, 1, -1, 768)))
+
