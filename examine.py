bf80eb3f72d597cef1dd6dd46db8dd333d8b008f
ayat29 <37980700+ayat29@users.noreply.github.com>
2023-09-29 10:52:41 +0600

    Update examine.py

--- examine.py
+++ examine.py
@@ -3,7 +3,7 @@ import torch
 from geospatial_fm import TemporalViTEncoder
 
 pretrained_path = ""
-model = TemporalViTEncoder(pretrained = pretrained_path)
+model = TemporalViTEncoder(pretrained = pretrained_path, embed_dim = 768)
 example_input_1 = torch.tensor(np.random.randn(1, 3, 1, 224, 224)).float()
 example_input_2 = torch.tensor(np.random.randn(1, 3, 1, 224, 224)).float()
 
