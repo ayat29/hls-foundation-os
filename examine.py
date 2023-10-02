de83787cfe1f513d0fc7edfd848b34fbbbbc6539
ayat29 <37980700+ayat29@users.noreply.github.com>
2023-10-02 13:41:00 +0600

    Update examine.py

--- examine.py
+++ examine.py
@@ -6,11 +6,11 @@ pretrained_path = "/content/drive/MyDrive/Prithvi_weights/Prithvi_100M.pt"
 model = TemporalViTEncoder(pretrained = pretrained_path, embed_dim = 768)
 nodel = ConvTransformerTokensToEmbeddingNeck(embed_dim = 768, output_embed_dim = 768, Hp = 14, Wp = 14)
 
-example_input_1 = torch.tensor(np.random.randn(1, 3, 1, 224, 224)).float()
-example_input_2 = torch.tensor(np.random.randn(1, 3, 1, 224, 224)).float()
+example_input_1 = torch.tensor(np.random.randn(1, 6, 1, 224, 224)).float()
+#example_input_2 = torch.tensor(np.random.randn(1, 3, 1, 224, 224)).float()
 
-out = model(example_input_1, example_input_2)
-out = nodel(out)
+out = model(example_input_1)
+#out = nodel(out)
 print(out)
 print(out.shape)
 #temp = out
