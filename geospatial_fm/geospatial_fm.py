2d2137f0880000deec9421c738b9443b51680447
ayat29 <37980700+ayat29@users.noreply.github.com>
2023-10-02 08:33:27 +0600

    Update geospatial_fm.py

--- geospatial_fm/geospatial_fm.py
+++ geospatial_fm/geospatial_fm.py
@@ -487,7 +487,7 @@ class TemporalViTEncoder(nn.Module):
     def forward(self, w):
         # embed patches
         print(w.shape)
-        x, y = w[:, :3, :, :, :].reshape(224, 224, 3), w[:, 3:, :, :, :].reshape(224, 224, 3)
+        x, y = w[:, :3, :, :, :], w[:, 3:, :, :, :]
         x, _, _ = self.patch_embed(x)
         y, _, _ = self.patch_embed(y)
 
